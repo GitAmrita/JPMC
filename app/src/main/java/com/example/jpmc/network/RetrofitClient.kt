72c43abb0314f753fc30e5ac2a7c89ebac51307e
Amrita Chowdhury <amc@amritas-mbp.lan>
2022-09-23 15:43:35 -0700

    check PAT

--- app/src/main/java/com/example/jpmc/network/RetrofitClient.kt
+++ app/src/main/java/com/example/jpmc/network/RetrofitClient.kt
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
+    // base url
     private const val BASE_URL = "https://data.cityofnewyork.us/"
     private const val TIME_OUT = 60L
 
